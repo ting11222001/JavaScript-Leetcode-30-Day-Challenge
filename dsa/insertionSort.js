7c8bb7fb5af874839c158bc5bab8a05393ca1926
tiffany <ting11222001@gmail.com>
2024-05-02 09:39:17 +0930

    Update insertion sort.

--- dsa/insertionSort.js
+++ dsa/insertionSort.js
@@ -14,7 +14,7 @@ function insertionSort(arr) {
     let j
 
     for (j = i - 1; j >= 0 && arr[j] > curr; j--) {
-      arr[j + 1] = arr[j] // move the big left to the curr i.e. on its right
+      arr[j + 1] = arr[j] // move the big left to the right
     }
 
     arr[j + 1] = curr // put the curr in front of the value when arr[j] < curr
@@ -55,12 +55,12 @@ insertionSort2([2, 4, 1, 3]) // [1, 2, 3, 4]
 // Solution 3:
 function insertionSort3(arr) {
   for (let i = 1; i < arr.length; i++) {
-    valueToSort = arr[i]
 
-    // arr[i - 1] is the value on the left, and keep going left
-    while (arr[i - 1] > valueToSort && i > 0) {
-      let temp = valueToSort
-      valueToSort = arr[i - 1]
+    // arr[i - 1] is the value on the left, and should keep going left
+    while (arr[i - 1] > arr[i] && i > 0) {
+      // SWAP
+      let temp = arr[i]
+      arr[i] = arr[i - 1]
       arr[i - 1] = temp
       i--
     }
@@ -72,3 +72,23 @@ console.log("Solution 3:")
 insertionSort3([2, 1, 3, 7, 5]) // [1, 2, 3, 5, 7]
 insertionSort3([5, 2, 4, 6, 1, 3]) // [1, 2, 3, 4, 5, 6]
 insertionSort3([2, 4, 1, 3]) // [1, 2, 3, 4]
+
+
+// Solution 4:
+function insertionSort4(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    // arr[i] is the value to sort
+    // arr[i - 1] is the value on the left, and keep going left
+    while (arr[i - 1] > arr[i] && i > 0) {
+      // SWAP shorthand
+      [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]]
+      i--
+    }
+  }
+  console.log(arr)
+}
+
+console.log("Solution 4:")
+insertionSort4([2, 1, 3, 7, 5]) // [1, 2, 3, 5, 7]
+insertionSort4([5, 2, 4, 6, 1, 3]) // [1, 2, 3, 4, 5, 6]
+insertionSort4([2, 4, 1, 3]) // [1, 2, 3, 4]
