0726fb77f0aeadfa58e50ab9a7524a7f1d897a41
tiffany <ting11222001@gmail.com>
2024-05-06 13:08:22 +0930

    Add array practice.

--- leetcode/Array/MaximumSubarray.js
+++ leetcode/Array/MaximumSubarray.js
@@ -1,3 +1,21 @@
 /**
  * https://www.youtube.com/watch?v=5WZl3MMT0Eg
  */
+
+function main(arr) {
+  let maxSubArrSum = arr[0]
+  let currSum = 0
+
+  for (let num of arr) {
+    if (currSum < 0) {
+      currSum = 0
+    }
+    currSum += num
+    maxSubArrSum = Math.max(maxSubArrSum, currSum)
+  }
+
+  return maxSubArrSum
+}
+
+const input = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+console.log(main(input)); // 6
--- leetcode/Array/twoSum.js
+++ leetcode/Array/twoSum.js
@@ -1,3 +1,24 @@
 /**
+ * https://www.youtube.com/watch?v=KLlXCFG5TnA&list=PLot-Xpze53ldVwtstag2TL4HQhAnC8ATf&ab_channel=NeetCode
  * 
+ * https://stackoverflow.com/questions/2966947/dynamically-add-data-to-a-javascript-map
+ * 
+ * https://stackoverflow.com/questions/1098040/checking-if-a-key-exists-in-a-javascript-object
  */
+
+function main(arr, target) {
+  let prevMap = {}  // val : index
+
+  for (let i = 0; i < arr.length; i++) { // i: 1, num: 7
+    let search = target - arr[i] // 9 - 7 = 2
+    if (search in prevMap) {
+      return [prevMap[search], i]
+    } else {
+      prevMap[arr[i]] = i // {2: 0}
+    }
+  }
+}
+
+const inputs = [2, 7, 11, 15]
+const target = 9
+console.log(main(inputs, target)); // [0, 1]
