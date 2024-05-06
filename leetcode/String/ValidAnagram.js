cc5dcc95074d84db8914f3e52ca6e82c6f7d9c56
tiffany <ting11222001@gmail.com>
2024-05-06 17:32:59 +0930

    Add string exercise from Blind 75

--- leetcode/String/ValidAnagram.js
+++ leetcode/String/ValidAnagram.js
@@ -1,3 +1,37 @@
 /**
  * https://www.youtube.com/watch?v=9UtInBqnCgA
  */
+
+function solution1(s, t) {
+  if (s.length !== t.length) return false
+  let countS = {} // {char: counts}
+  let countT = {}
+
+  for (let i = 0; i < s.length; i++) {
+    countS[s[i]] = countS[s[i]] ? countS[s[i]] + 1 : 1
+    countT[t[i]] = countT[t[i]] ? countT[t[i]] + 1 : 1
+  }
+
+  for (let char in countS) {
+    if (countS[char] !== countT[char]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+const s1 = "anagram"
+const t1 = "nagaram"
+console.log(solution1(s1, t1)); // true
+
+const s2 = "rat"
+const t2 = "car"
+console.log(solution1(s2, t2)); // false
+
+function solution2(s, t) {
+  return s.split('').sort().join('') === t.split('').sort().join('') // make strings into arrays before sorting
+}
+
+console.log(solution2(s1, t1)); // true
+console.log(solution2(s2, t2)); // false
--- leetcode/String/ValidPalindrome.js
+++ leetcode/String/ValidPalindrome.js
@@ -49,4 +49,37 @@ console.log(palindrome2("school")); // false
 
 /**
  * https://www.youtube.com/watch?v=jJXJ16kPFWg
+ * only consider A-Z, a-z, 0-9 the alphanumerical characters
+ * 
+ * https://www.youtube.com/watch?v=P1gmutIKPRU&ab_channel=JSDev
+ * https://stackoverflow.com/questions/4434076/best-way-to-alphanumeric-check-in-javascript
  */
+// I should consider if the inputs have other characters, e.g. whitespace, etc.
+const s = "A man, a plan, a canal: Panama"
+const t = "race a car"
+
+function check(str) {
+  let left = 0
+  let right = str.length - 1
+
+  while (left <= right) {
+    if (str[left] === str[right]) {
+      left++
+      right--
+    } else {
+      return false
+    }
+  }
+
+  return true
+}
+
+function solution3(str) {
+  const regex = /[^a-zA-Z0-9]/g // negate anything that's not alphanumerical and use g to capture all
+  const clean = str.replace(regex, '').toLowerCase()
+
+  return check(clean)
+}
+
+console.log(solution3(s)); // true
+console.log(solution3(t)); // false
